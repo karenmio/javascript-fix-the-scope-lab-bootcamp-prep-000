e5a1dbdba63d2c3745e744fcf4fbb5aff913b249
karen wang <karenkarenmio@gmail.com>
2018-05-05 20:59:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ function add2(n) {
   return n + two;
 }
 
-var funkyFunction = function() {
-  return function() {
+var funkyFunction = function() { return function() {
     return "FUNKY!"
   }
 }
@@ -28,6 +27,6 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 
-var theFunk = funkyFunction.console.log();
+var theFunk = console.log(funkyFunction)
 
 
